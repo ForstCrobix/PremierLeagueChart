25f70d8746ee95332e65f7f85ede4437074fda74
rob.dmind@gmail.com <philip10cc>
2016-05-22 19:25:53 +0100

    Added button for signing in

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,33 +3,27 @@
 
 <head>
   <link rel="stylesheet" type="text/css" href="Full_Page/jquery.fullPage.css" />
-   <script src="components/jquery.min.js"></script>
-   <script type="text/javascript" src="Full_Page/jquery.fullPage.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <script src="components/jquery.min.js"></script>
+  <script type="text/javascript" src="Full_Page/jquery.fullPage.js"></script>
 
    <script type="text/javascript">
    $(document).ready(function() {
            $('#fullpage').fullpage({
-               sectionsColor: ['#ff9933', '#0099ff', '#00cc66'],
+               sectionsColor: ['#ff9933', '#0099ff', '#00cc66']
            });
        });
     </script>
-
-    <style>
-         .section{
-             font-size: 6em;
-             text-align: center;
-         }
-     </style>
 </head>
 
 <body>
+    <button id="sign-in-button" type="button">Sign in</button>
     <div id="fullpage">
         <div class="section">Rob...</div>
         <div class="section">
             <div class="slide">1</div>
             <div class="slide">2</div>
         </div>
-
         <div class="section">...COOL!!</div>
     </div>
 </body>
