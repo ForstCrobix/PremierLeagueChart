f75882655726da1910cf74954bfe959721b1a2c4
ForstCrobix <rob.dmind@gmail.com>
2016-05-22 19:23:44 +0100

    Added top buttons and side scrolling option

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,12 @@
    <script type="text/javascript">
    $(document).ready(function() {
            $('#fullpage').fullpage({
-               sectionsColor: ['#ff9933', '#0099ff', '#00cc66'],
+                sectionsColor: ['#ff9933', '#0099ff', '#00cc66'],
+                menu: '#menu',
+                anchors: ['firstPage', 'secondPage', 'thirdPage'],
+                navigation: true,
+ 				        navigationPosition: 'right',
+ 				        navigationTooltips: ['Dashboard', 'Upcoming', 'Table']
            });
        });
     </script>
@@ -19,18 +24,69 @@
              font-size: 6em;
              text-align: center;
          }
-     </style>
+
+            #menu{
+              background-color: transparent;
+            	position:fixed;
+            	top:0;
+            	left:0;
+            	height: 40px;
+            	z-index: 70;
+            	width: 100%;
+            	padding: 0;
+            	margin:0;
+            }
+
+            #menu li {
+            	display:inline-block;
+            	margin:10px;
+              margin-right: 0px !important;
+            	color: #000;
+            	background:#fff;
+            	background: rgba(255,255,255, 0.5);
+            	-webkit-border-radius: 10px;
+                        border-radius: 10px;
+
+            }
+            #menu li.active{
+            	background:#666;
+            	background: rgba(0,0,0, 0.5);
+            	color: #fff;
+            }
+            #menu li a{
+            	text-decoration:none;
+            	color: #000;
+            }
+            #menu li.active a:hover{
+            	color: #000;
+            }
+            #menu li:hover{
+            	background: rgba(255,255,255, 0.8);
+            }
+            #menu li a,
+            #menu li.active a{
+            	padding: 9px 18px;
+            	display:block;
+            }
+            #menu li.active a{
+            	color: #fff;
+            }
+
+
+    </style>
 </head>
 
 <body>
-    <div id="fullpage">
-        <div class="section">Rob...</div>
-        <div class="section">
-            <div class="slide">1</div>
-            <div class="slide">2</div>
-        </div>
+    <ul id="menu">
+      <li data-menuanchor="firstPage" class="active"><a href="#firstPage">P1P1P1P1</a></li>
+      <li data-menuanchor="secondPage"><a href="#secondPage">P2P2P2P2</a></li>
+      <li data-menuanchor="thirdPage"><a href="#thirdPage">P3P3P3P3</a></li>
+    </ul>
 
-        <div class="section">...COOL!!</div>
+    <div id="fullpage">
+        <div class="section" data-anchor="firstPage">Rob...</div>
+        <div class="section" data-anchor="secondPage">...is...</div>
+        <div class="section" data-anchor="thirdPage">...COOL!!</div>
     </div>
 </body>
 </html>
