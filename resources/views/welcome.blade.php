b4f9d96e2a8fd787a213077c618178c266b187c7
rob.dmind@gmail.com <philip10cc>
2016-05-22 20:27:23 +0100

    Combined all the fixed buttons at the top of the sections and added a new font. Also added a style.css to put css in future.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,11 @@
 <html>
 
 <head>
-  <link rel="stylesheet" type="text/css" href="Full_Page/jquery.fullPage.css" />
-  <link rel="stylesheet" type="text/css" href="style.css" />
-  <script src="components/jquery.min.js"></script>
-  <script type="text/javascript" src="Full_Page/jquery.fullPage.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Josefin+Slab' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="Full_Page/jquery.fullPage.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <script src="components/jquery.min.js"></script>
+    <script type="text/javascript" src="Full_Page/jquery.fullPage.js"></script>
 
    <script type="text/javascript">
    $(document).ready(function() {
@@ -23,17 +24,14 @@
 
 <body>
     <ul id="menu">
-        <li data-menuanchor="firstPage" class="active"><a href="#firstPage">P1P1P1P1</a></li>
-        <li data-menuanchor="secondPage"><a href="#secondPage">P2P2P2P2</a></li>
-        <li data-menuanchor="thirdPage"><a href="#thirdPage">P3P3P3P3</a></li>
+        <li data-menuanchor="firstPage" class="active"><a href="#firstPage">Overview</a></li>
+        <li data-menuanchor="secondPage"><a href="#secondPage">Predictions</a></li>
+        <li data-menuanchor="thirdPage"><a href="#thirdPage">Chart</a></li>
     </ul>
     <button id="sign-in-button" type="button">Sign in</button>
     <div id="fullpage">
         <div class="section">Rob...</div>
-        <div class="section">
-            <div class="slide">1</div>
-            <div class="slide">2</div>
-        </div>
+        <div class="section">...is...</div>
         <div class="section">...COOL!!</div>
     </div>
 </body>
